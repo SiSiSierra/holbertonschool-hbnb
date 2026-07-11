d45d14d2000ee6649407d106e8c13cc45423bc69
SSabin0 <13078@holbertonstudents.com>
2026-07-12 02:57:46 +1000

    Add new amenities to data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -7,3 +7,12 @@ VALUES (
     '$2b$12$WWE5SxHwqf1jObUshpuCbeIY5dyn3DAHcrSk8s2ALuMV/a270zHAO',
     TRUE
 );
+
+INSERT INTO Amenity (id, name)
+VALUES ('60a09432-8998-44f7-9678-a3fd2b9353fc', 'WiFi');
+
+INSERT INTO Amenity (id, name)
+VALUES ('fc146d22-2a43-48b3-bbfc-b8202e184786', 'Swimming Pool');
+
+INSERT INTO Amenity (id, name)
+VALUES ('5630431b-b822-49fa-a628-7d9ea03eff4f', 'Air Conditioning');
