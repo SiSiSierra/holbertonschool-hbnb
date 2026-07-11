f685bbdadadb63f99a62e2b0e56d7eb2238f8d50
SSabin0 <13078@holbertonstudents.com>
2026-07-12 02:55:30 +1000

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,1 +1,9 @@
-
+INSERT INTO User (id, first_name, last_name, email, password, is_admin)
+VALUES (
+    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+    'Admin',
+    'HBnB',
+    'admin@hbnb.io',
+    '$2b$12$WWE5SxHwqf1jObUshpuCbeIY5dyn3DAHcrSk8s2ALuMV/a270zHAO',
+    TRUE
+);
