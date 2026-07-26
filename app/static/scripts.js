e9981a376699a113f6f5f96fb51faad30c13db3a
Sierra <SierraHunt@protonmail.com>
2026-07-27 02:29:04 +1000

    Implemented place details page

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -1,8 +1,3 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
-
 // Get a cookie by a given name and return content
 function getCookieByName(name) {
   const cookies = document.cookie.split(';');
@@ -24,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('login-link').remove();
   }
 
+
   // Add login functionality if this page is login.html
   const loginForm = document.getElementById('login-form');
   if (loginForm) {
@@ -34,6 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
       loginUser(form.elements['email'].value, form.elements['password'].value)
     });
   }
+
 
   // Run this if page is index
   const index = document.getElementById('places-list');
@@ -69,6 +66,19 @@ document.addEventListener('DOMContentLoaded', () => {
     getAllPlaces();
   }
 
+
+  // Run this if page is place detail
+  const detail = document.getElementById('place-details')
+  if (detail) {
+    // Disable review box if no login cookie
+    const cookie = getCookieByName('token');
+    if (!cookie) {
+      document.getElementById('add-review').style.display = 'None';
+    }
+    // Load place details from ID
+    const param = window.location.search.split("=")[1];
+    getPlace(param);
+  }
 });
 
 //
@@ -97,7 +107,6 @@ async function loginUser(email, password) {
   }
 }
 
-
 // Send request for all places from api
 async function getAllPlaces() {
   const headerObj = {
@@ -121,42 +130,182 @@ async function getAllPlaces() {
     message.className = 'place-card';
     message.textContent = 'There are no places here yet!';
     section.appendChild(message);
-  } else {
-    // Make unordered list
-    const list = document.createElement('ul');
-    // Make item for each place in list
-    for (let i = 0; i < data.length; i++) {
-      const item = document.createElement('li');
-      // Title
-      const title = document.createElement('h2');
-      title.textContent = data[i].title;
-      // Price
-      const price = document.createElement('p');
-      price.textContent = 'Price per night: $' + data[i].price;
-      // Details
-      const details = document.createElement('a');
-      details.textContent = 'View Details'
-      details.href = 'place'
-      item.appendChild(title);
-      item.appendChild(price);
-      item.appendChild(details);
-      item.className = 'place-card';
-      // Set id depending on price for use with filter
-      priceNum = parseInt(data[i].price);
-      if (priceNum > 100) {
-        item.classList.add('All');
-      } else if (priceNum > 50) {
-        item.classList.add('100');
-      } else if (priceNum > 10) {
-        item.classList.add('50');
-      } else {
-        item.classList.add('10');
-      }
-      list.appendChild(item);
-    }
-    // Add unordered list to section
-    section.appendChild(list);
+    return;
   }
+
+  // Make unordered list
+  const list = document.createElement('ul');
+  // Make item for each place in list
+  for (let i = 0; i < data.length; i++) {
+    const item = document.createElement('li');
+    // Title
+    const title = document.createElement('h2');
+    title.textContent = data[i].title;
+    // Price
+    const price = document.createElement('p');
+    price.textContent = 'Price per night: $' + data[i].price;
+    // Details
+    const details = document.createElement('a');
+    details.textContent = 'View Details';
+    details.href = 'place?id=' + data[i].id;
+    details.className = 'details-button';
+    item.appendChild(title);
+    item.appendChild(price);
+    item.appendChild(details);
+    item.className = 'place-card';
+    // Set id depending on price for use with filter
+    priceNum = parseInt(data[i].price);
+    if (priceNum > 100) {
+      item.classList.add('All');
+    } else if (priceNum > 50) {
+      item.classList.add('100');
+    } else if (priceNum > 10) {
+      item.classList.add('50');
+    } else {
+      item.classList.add('10');
+    }
+    list.appendChild(item);
+  }
+  // Add unordered list to section
+  section.appendChild(list);
 }
 
+
+// Send request for one place from API
+async function getPlace(id) {
+  const headerObj = {
+    'Content-Type': 'application/json'
+  };
+  // Add JWT if we have it in cookie
+  const cookie = getCookieByName('token');
+  if (cookie) {
+    headerObj['Authorizaton'] = 'bearer ' + cookie;
+  }
+  // Send fetch
+  const placeResponse = await fetch('http://localhost:5000/api/v1/places/' + id, {
+    method: 'GET',
+    headers: headerObj
+  })
+  // Handle response
+  section1 = document.getElementById('place-details');
+  // Error response
+  if (placeResponse.status !== 200) {
+    const message = document.createElement('h2');
+    message.textContent = 'The requested place could not be found.';
+    section1.appendChild(message);
+    return;
+  }
+
+
+  const placeData = await placeResponse.json();
+  console.log(placeData);
+  const placeCard = document.createElement('dl');
+  placeCard.id = 'place-info';
+
+  // Add place info elements
+  // Heading
+  const title = document.createElement('h1');
+  title.textContent = placeData.title;
+  section1.appendChild(title);
+  // Host
+  const hostt = document.createElement('dt');
+  hostt.textContent = 'Host:';
+  const hostd = document.createElement('dd');
+  hostd.textContent = placeData.owner.first_name + ' ' + placeData.owner.last_name;
+  // Price
+  const pricet = document.createElement('dt');
+  pricet.textContent = 'Price:';
+  const priced = document.createElement('dd');
+  priced.textContent = placeData.price;
+  // Description
+  const desct = document.createElement('dt');
+  desct.textContent = 'Description:';
+  const descd = document.createElement('dd');
+  descd.textContent = placeData.description;
+  // Amenities
+  const ament = document.createElement('dt');
+  ament.textContent = 'Amenities:';
+  const amend = document.createElement('dd');
+  // Build string based on amenities
+  string = '';
+  for (let i = 0; i < placeData.amenities.length; i++) {
+    if (i !== 0) {
+      string = string + ', ';
+    }
+    string = string + placeData.amenities[i];
+  }
+  if (placeData.amenities.length === 0) {
+    string = 'None';
+  }
+  amend.textContent = string;
+  placeCard.appendChild(hostt);
+  placeCard.appendChild(hostd);
+  placeCard.appendChild(pricet);
+  placeCard.appendChild(priced);
+  placeCard.appendChild(desct);
+  placeCard.appendChild(descd);
+  placeCard.appendChild(ament);
+  placeCard.appendChild(amend);
+  section1.appendChild(placeCard);
+
+
+  // Add reviews
+  const heading = document.createElement('h2');
+  heading.textContent = 'Reviews'
+  section2 = document.getElementById('reviews');
+  section2.appendChild(heading);
+  // fetch reviews
+  const reviewsResponse = await fetch('http://localhost:5000/api/v1/places/' + id + '/reviews', {
+    method: 'GET',
+    headers: headerObj
+  });
+  if (reviewsResponse.status !== 200) {
+    const message = document.createElement('h3');
+    message.textContent = 'Reviews could not be retrived';
+    section2.appendChild(message);
+    return;
+  }
+
+  const reviewsData = await reviewsResponse.json();
+  console.log(reviewsData);
+  if (reviewsData.length === 0) {
+    const message = document.createElement('h3');
+    message.textContent = 'This place has no reviews yet';
+    section2.appendChild(message);
+    return;
+  }
+  const reviewsList = document.createElement('ul');
+  // Iterate over and add reviews
+  for (let i = 0; i < reviewsData.length; i++) {
+    // Get user's name by fetching from ID
+    const userResponse = await fetch('http://localhost:5000/api/v1/users/' + reviewsData[i].user_id, {
+      method: 'GET',
+      headers: headerObj
+    });
+    if (userResponse.status !== 200) {
+      continue;
+    }
+    // populate review
+    const reviewCard = document.createElement('li');
+    reviewCard.className = 'review-card';
+    // Name
+    const userData = await userResponse.json();
+    console.log(userData);
+    const name = document.createElement('h3');
+    name.textContent = userData.first_name + ' ' + userData.last_name;
+    // Text
+    const text = document.createElement('p');
+    text.textContent = reviewsData[i].text;
+    // Rating
+    const rating = document.createElement('p');
+    rating.textContent = 'Rating: ' + reviewsData[i].rating + '/5';
+    // Add elements
+    reviewCard.appendChild(name);
+    reviewCard.appendChild(text);
+    reviewCard.appendChild(rating);
+    reviewsList.appendChild(reviewCard);
+  }
+  // Add list to section
+  section2.appendChild(reviewsList);
+}
 
